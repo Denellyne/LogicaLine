ff6a49d062e728f0ed39f0aaa50ee23c1d44c13a
Denellyne <gustavobarbosavs@gmail.com>
2025-05-12 11:04:09 +0100

    Read input without quotes or dot on the end of string and nicknames for users

--- src/client.pl
+++ src/client.pl
@@ -1,6 +1,19 @@
 :- use_module(library(socket)).
 :- use_module(library(readutil)).
 
+
+setup_client_ip(Ip,Port):-
+  setup_call_catcher_cleanup(
+    tcp_socket(Socket),
+    tcp_connect(Socket, Ip:Port),
+    exception(_),
+    tcp_close_socket(Socket)),
+  
+setup_call_cleanup(
+    tcp_open_socket(Socket, In, Out),
+    handle_connection(In, Out),
+    close_connection(In, Out)).
+
 setup_client(Port) :- 
   setup_call_catcher_cleanup(
     tcp_socket(Socket),
@@ -30,8 +43,9 @@ receive_messages(In) :-
 
 send_messages(Out) :-
     writeln("Input:"),
-    read(Input),nl,
-    writeln(Out,Input),
+    current_input(Input),
+    read_string(Input, "\n", "\r", _Sep, String),
+    writeln(Out,String),
     flush_output(Out),
     send_messages(Out).
 
--- src/server.pl
+++ src/server.pl
@@ -8,8 +8,8 @@ create_server(Port) :-
       stream_pair(StreamPair, AcceptFd, _),
       dispatch(AcceptFd,[]).
 
-
 :- dynamic connections/1.
+:- dynamic aliases/2.
 
 dispatch(AcceptFd,Connections) :-
         tcp_accept(AcceptFd, Socket, Peer),
@@ -18,21 +18,35 @@ dispatch(AcceptFd,Connections) :-
         dispatch(AcceptFd,Connections).
 
 
-process_client(Socket, _Peer) :-
+process_client(Socket, Peer) :-
     setup_call_cleanup(
-        tcp_open_socket(Socket, In,Out),
-        handle_client(In,Out),
+        tcp_open_socket(Socket,In,Out),
+        handle_client(In,Out,Peer),
         close_connection(In,Out)
     ).
 
 close_connection(In, Out) :-
         write("Closing stream"),
+        retract(connections(Out)),
         close(In, [force(true)]),
         close(Out, [force(true)]).
 
-handle_client(In,Out) :-
+
+handle_client(In,Out,Peer) :-
+  ip_name(Peer,Ip),
+  findall(X,aliases(Ip,X),Aliases),
+  ( Aliases == [] -> 
+  writeln(Out,"Input the alias from which you wish to be called by:"),
+  flush_output(Out),
+  set_stream(In,timeout(60)),
+  catch(read_line_to_string(In, Input),_, fail),
+  set_stream(In,timeout(infinite)),
+  assertz(aliases(Ip,Input));true),
+  
+  aliases(Ip,Alias),
   assertz(connections(Out)),
-  handle_service(In,Out).
+  string_concat(Alias,": ",Nickname),
+  handle_service(In,Out,Nickname).
 
 send_message_to_client(_,[]).
 send_message_to_client(Input,[Out|Connections]) :- 
@@ -41,16 +55,19 @@ send_message_to_client(Input,[Out|Connections]) :-
     flush_output(Out),
     send_message_to_client(Input,Connections).
   
-send_message(Input) :-
+send_message(Input,Out,Alias) :-
   bagof(X,connections(X),Connections),
-  send_message_to_client(Input,Connections).
+  delete(Connections,Out,ConnectionsParsed),
+
+  string_concat(Alias,Input,String),
+  send_message_to_client(String,ConnectionsParsed).
   
 
-handle_service(In,Out) :-
+handle_service(In,Out,Alias) :-
     read_line_to_string(In, Input),
     (  Input == end_of_file -> writeln("Connection dropped"),fail;
       writeln(Input),
-      send_message(Input),
-      handle_service(In,Out)
+      send_message(Input,Out,Alias),
+      handle_service(In,Out,Alias)
     ).
     
