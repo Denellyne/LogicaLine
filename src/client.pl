3bfc6e8d3425d14bbc52732ee4b227a40bb87f69
Denellyne <gustavobarbosavs@gmail.com>
2025-06-05 16:54:35 +0100

    a

--- src/client.pl
+++ src/client.pl
@@ -167,7 +167,10 @@ receive_messages(StreamPair) :-
                 writeln(SenderStream),
                 writeln(31),
                 findall(symmetric_keys(X,Y),symmetric_keys(X,Y),Keys),
+                
                 writeln(Keys),
+                writeln(StreamPair),
+                writeln(SenderStream),
                 symmetric_keys(SenderStream, SymmetricKey),
                 writeln("aqui"),
                 crypto_data_decrypt(EncryptedData, "aes-128-gcm" , SymmetricKey, IV, Decoded, [tag(Tag)]),
